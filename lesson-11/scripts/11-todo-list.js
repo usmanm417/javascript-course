bc4ed2cc82e735705067a07e3986e2903c3fde87
usmanm417 <usmanm417@gmail.com>
2024-07-02 09:20:24 -0400

    finished todo list

--- lesson-11/scripts/11-todo-list.js
+++ lesson-11/scripts/11-todo-list.js
@@ -1,4 +1,4 @@
-const todoList = ['something', 'something else'];
+const todoList = [];
 
 renderTodoList();
 
@@ -6,19 +6,42 @@ function renderTodoList() {
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        const name = todoObject.name;
+        const dueDate = todoObject.dueDate;
+        //this next line is a shortcut for the two lines above
+        //const {name, dueDate} = todoObject;
+        const html = `
+            <div><input type="checkbox">${name}</div>
+            <div>${dueDate}</div>
+            <button onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>
+        </div>`;
         todoListHTML += html;
     }
-    console.log(todoListHTML);
 
     document.querySelector('.js-todo-list')
         .innerHTML = todoListHTML;
 }
+
+function addTodoKeydown(event) {
+    if (event.key == 'Enter') {
+        addTodo();
+    }
+}
+
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
+    const dateInputElement = document.querySelector('.js-due-date-input');
     const name = inputElement.value;
-    todoList.push(name);
+    const dueDate = dateInputElement.value;
+    todoList.push({
+        name: name,
+        dueDate: dueDate
+        //name, dueDate (this line does the same as the above 2)
+    });
     inputElement.value = '';
     renderTodoList();
 }
